5504c3f3d6906baee369c70fa8ced1f7ea9e5672
Benjamin Harcourt <Benjamin Harcourt>
2016-07-20 15:37:28 -0400

    changes made to workout log

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,21 @@
 var router=require('express').Router();
 var sequelize=require('../db.js');
 var User=sequelize.import('../models/user');
+var bcrypt=require('bcryptjs');
+var jwt=require('jsonwebtoken');
 router.post('/', function(req, res){
 	var username=req.body.user.username;
 	var pass=req.body.user.password; //TODO: make it hashed
 	User.create({
 		username:username,
-		passwordHash: '' //TODO: make it hashed
+		passwordHash: bcrypt.hashSync(pass,10) //TODO: make it hashed
 	}).then(
 	function createSuccess(user){
+		var token=jwt.sign({id:user.id}, "Shhhh I'm a secret", {expiresIn: 60*60*24});
 		res.json({
 			user:user,
-			message: 'created'
+			message: 'created',
+			sessionToken: token
 		});
 	},
 	function createError(err){
