4ee449e5eff4903250dca60753c4fdf7b6a25c16
Benjamin Harcourt <Benjamin Harcourt>
2016-07-21 13:06:30 -0400

    creating definitions and routes

--- server/models/definition.js
+++ server/models/definition.js
@@ -1,4 +1,11 @@
 //definition model needs description, logtype, and an owner
 module.exports=function(sequelize, Datatypes){
+	var Definition=sequelize.define('definition',{
+		description: DataTypes.STRING,
+		logType: DataTypes.String,
+		owner: DataTypes.INTEGER
 
+
+	});
+	return Definition;
 };
